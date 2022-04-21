d4a007709e530b1739d6e000490e5e009af72118
Owen Brush <owenbrush@gmail.com>
2022-04-20 20:57:20 -0700

    created custom transformers script for data pipelines

---
+++ custom_transformers.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from string import punctuation
+
+EXPRESSIONS_TO_REMOVE = ["\\"+x for x in list(punctuation)]
+EXPRESSIONS_TO_REMOVE.remove('\\!')
+
+
+class FeatureBuilder(BaseEstimator, TransformerMixin):
+    def __init__(self, models:list = None, min_characters_for_wordcount:int = 1):
+        self.min_characters_for_wordcount = min_characters_for_wordcount
+
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X:pd.DataFrame, y:pd.DataFrame = None):
+        X = X.copy()
+        BooleanFeatures(X)
+        CleanStrings(X)
+        CountStrings(X, self.min_characters_for_wordcount)
+        
+        if y is None:
+            return X
+        else:
+            y = y.copy()
+            y = y.str.lower().str.strip()   
+            return X, y
+        
+def BooleanFeatures(X:pd.DataFrame):
+    X['noun_involved'] = X['clue'].str.contains('[A-Z].*[A-Z]',regex=True)
+    X['fill_blank'] = X['clue'].str.contains('_', regex=False)
+    return X
+
+def CleanStrings(X:pd.DataFrame):
+    X['clue'] = X['clue'].str.lower().str.strip()
+    X['clue'] = X['clue'].replace('$', ' money ', regex=False)
+    X['clue'] = X['clue'].replace('!', ' ! ', regex=False)
+    X['clue'] = X['clue'].replace('``', '"', regex=False)
+    X['clue'] = X['clue'].replace(r'\b\w{1,1}\b','', regex=True) 
+    X['clue'] = X['clue'].replace(EXPRESSIONS_TO_REMOVE, ' ',regex=True)
+    X['clue'] = X['clue'].replace('\d+', '', regex=True)
+    X['clue'] = X['clue'].replace(' +', ' ', regex=True)
+    X['clue'] = X['clue'].replace(['nan',''], np.nan, regex=False)
+    return X
+        
+def CountStrings(X:pd.DataFrame, min_characters_for_wordcount:int):
+    X['word_count'] = X['clue'].str.count(f'\\b\\w{{{min_characters_for_wordcount},}}') 
+    X['answer_length'] = X['answer_characters'].str.len()
+    return X
+
+    
+    
+class CosineSimilarity(BaseEstimator, TransformerMixin):
+    
+    def __init__(self, gensim_models:list):
+        self.gensim_models = gensim_models
+        
+    def set_word2vec_models(self, gensim_models:list):
+        self.gensim_models = gensim_models
+    
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X, y = None):
+        for model in self.gensim_models:
+           pass
+        if y is None:
+            return X
+        else: 
+            return X, y
