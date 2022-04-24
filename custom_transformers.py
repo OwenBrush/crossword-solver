f5a9ccb7c88710fe39ca48d257420039a35f9564
Owen Brush <owenbrush@gmail.com>
2022-04-24 10:49:15 -0700

    Refactoring

--- custom_transformers.py
+++ custom_transformers.py
@@ -10,34 +10,42 @@ EXPRESSIONS_TO_REMOVE.remove('\\!')
 
 
 class FeatureBuilder(BaseEstimator, TransformerMixin):
-    def __init__(self, models:list = None, min_characters_for_wordcount:int = 1, remove_stop_words=True):
-        self.min_characters_for_wordcount = min_characters_for_wordcount
-        self.remove_stop_words = remove_stop_words
+    def __init__(   self,
+                    boolean_features=True,
+                    clean_strings=True,
+                    parse_stopwords=True,
+                    count_strings=True,
+                    min_characters:int = 1):
+        
+        self.boolean_features = boolean_features
+        self.clean_strings = clean_strings
+        self.parse_stopwords = parse_stopwords
+        self.count_strings = count_strings
+        self.min_characters = min_characters
+
 
     def fit(self, X, y = None):
         return self
 
     def transform(self, X:pd.DataFrame, y:pd.DataFrame = None):
         X = X.copy()
-        BooleanFeatures(X)
-        CleanStrings(X)
-        if self.remove_stop_words:
-            ParseStopwords(X)
-        CountStrings(X, self.min_characters_for_wordcount)
-        
+        if self.boolean_features: boolean_features(X)
+        if self.clean_strings: clean_strings(X)
+        if self.parse_stopwords:parse_stopwords(X)
+        if self.count_strings :count_strings(X, self.min_characters)        
         if y is None:
             return X
         else:
             y = y.copy()
-            y = y.str.lower().str.strip()   
+            if self.clean_strings: y = y.str.lower().str.strip()   
             return X, y
         
-def BooleanFeatures(X:pd.DataFrame):
-    X['noun_involved'] = X['clue'].str.contains('[A-Z].*[A-Z]',regex=True)
-    X['fill_blank'] = X['clue'].str.contains('_', regex=False)
+def boolean_features(X:pd.DataFrame):
+    X['noun_involved'] = X['clue'].str.contains('[A-Z].*[A-Z]',regex=True).astype(float).astype('Int64')
+    X['fill_blank'] = X['clue'].str.contains('_', regex=False).astype(float).astype('Int64')
     return X
 
-def CleanStrings(X:pd.DataFrame):
+def clean_strings(X:pd.DataFrame):
     X['clue'] = X['clue'].str.lower().str.strip()
     X['clue'] = X['clue'].replace('$', ' money ', regex=False)
     X['clue'] = X['clue'].replace('!', ' ! ', regex=False)
@@ -49,12 +57,12 @@ def CleanStrings(X:pd.DataFrame):
     X['clue'] = X['clue'].replace(['nan',''], np.nan, regex=False)
     return X
         
-def CountStrings(X:pd.DataFrame, min_characters_for_wordcount:int):
+def count_strings(X:pd.DataFrame, min_characters_for_wordcount:int):
     X['word_count'] = X['clue'].str.count(f'\\b\\w{{{min_characters_for_wordcount},}}') 
     X['answer_length'] = X['answer_characters'].str.len()
     return X
 
-def ParseStopwords(X:pd.DataFrame):
+def parse_stopwords(X:pd.DataFrame):
     clues_without_stops = X['clue'].astype(str).apply(remove_stopwords)
     filter = clues_without_stops != ''
     X.loc[filter, 'clue'] = clues_without_stops[filter] 
