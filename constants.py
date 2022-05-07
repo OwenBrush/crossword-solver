5115372582434ed6dbeffec4ef106b3c72e4b776
Owen Brush <owenbrush@gmail.com>
2022-05-07 11:06:17 -0700

    created visualization of results and update previous graphs

--- constants.py
+++ constants.py
@@ -9,3 +9,11 @@ PCA_TEST = 'pca_test_data.csv'
 PREDICTED_SIMILARITIES = 'cosine_test_data.csv'
 SAMPLE_SIZE = 50000
 RANDOM_STATE = 357394
+
+KNOWN_CHARACTER_SETTINGS = ['0.0%_known_characters','20.0%_known_characters','40.0%_known_characters'] # Columns of known characters to use for prediction trials
+
+COLOR_1 = '#944DA2'
+COLOR_2 = '#40B0A6'
+COLOR_3 = '#BF4F1F'
+COLOR_4 = '#E1BE6A'
+COLOR_5 = '#790900'
